08c8372e39cfa4471210e2361c0ba8c8bd09c466
Le Tieu Phi <phile@24i.com>
2024-09-17 16:22:54 +0700

    Init the project. Set up migrations and stuffs.

---
+++ quiz_game/database/migrations/2024_09_17_085940_create_user_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('question_id')->constrained('questions')->onDelete('cascade');
+            $table->foreignId('selected_option_id')->nullable()->constrained('options')->onDelete('set null');
+            $table->timestamp('submitted_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_answers');
+    }
+};
